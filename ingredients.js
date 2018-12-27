6a2090d195fb1636d48c665c9533044af9591791
pigbog <mesteves@gmail.com>
2018-12-27 14:30:38 +0000

    Add ingredients loop excercise

---
+++ ingredients.js
@@ -0,0 +1,6 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+for (var i =0; i < ingredients.length; i++){
+  console.log(ingredients[i])
+}
+
